4093e3c27488ae57047064f5f98550ed0442ada5
Leslie Aula <aulaleslie@gmail.com>
2024-08-05 07:33:41 +0800

    chore: add edit and handle role changing capability

--- Modules/User/Resources/views/users/partials/actions.blade.php
+++ Modules/User/Resources/views/users/partials/actions.blade.php
@@ -1,14 +1,15 @@
-<a href="{{ route('users.edit', $data->id) }}" class="btn btn-info btn-sm">
+<a href="{{ route('users.edit', $data->user_id) }}" class="btn btn-info btn-sm">
     <i class="bi bi-pencil"></i>
 </a>
 <button id="delete" class="btn btn-danger btn-sm" onclick="
     event.preventDefault();
     if (confirm('Anda Yakin untuk Menghapus? Data akan Terhapus Permanen!')) {
-    document.getElementById('destroy{{ $data->id }}').submit();
+    document.getElementById('destroy{{ $data->user_id }}').submit();
     }
     ">
     <i class="bi bi-trash"></i>
-    <form id="destroy{{ $data->id }}" class="d-none" action="{{ route('users.destroy', $data->id) }}" method="POST">
+    <form id="destroy{{ $data->id }}" class="d-none" action="{{ route('users.destroy', $data->user_id) }}"
+          method="POST">
         @csrf
         @method('delete')
     </form>
