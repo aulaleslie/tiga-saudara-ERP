6847faa6681f66cd56e2448a88fcefaa951fbe77
Leslie Aula <aulaleslie@gmail.com>
2024-08-24 03:26:28 +0800

    chore: add transaction history

--- Modules/Product/Resources/views/products/show.blade.php
+++ Modules/Product/Resources/views/products/show.blade.php
@@ -13,9 +13,9 @@
 @section('content')
     <div class="container-fluid mb-4">
         <div class="row">
-            <!-- Left Section -->
-            <div class="col-lg-9 d-flex flex-column">
-                <div class="card flex-grow-1">
+            <!-- Left Section (Product Details) -->
+            <div class="col-lg-9">
+                <div class="card">
                     <div class="card-body">
                         <div class="table-responsive">
                             <table class="table table-bordered table-striped mb-0">
@@ -82,51 +82,11 @@
                         </div>
                     </div>
                 </div>
-
-                <!-- Transaction History -->
-                <div class="card mt-4 flex-grow-1">
-                    <div class="card-header">
-                        <h5>Transaction History</h5>
-                    </div>
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table class="table table-bordered table-striped">
-                                <thead>
-                                <tr>
-                                    <th>Date</th>
-                                    <th>Type</th>
-                                    <th>Quantity</th>
-                                    <th>Current Quantity</th>
-                                    <th>Location</th>
-                                    <th>Reason</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @forelse($product->transactions as $transaction)
-                                    <tr>
-                                        <td>{{ $transaction->formatted_created_at }}</td>
-                                        <td>{{ $transaction->type }}</td>
-                                        <td>{{ $transaction->quantity }}</td>
-                                        <td>{{ $transaction->current_quantity }}</td>
-                                        <td>{{ $transaction->location->name ?? 'N/A' }}</td>
-                                        <td>{{ $transaction->reason ?? 'N/A' }}</td>
-                                    </tr>
-                                @empty
-                                    <tr>
-                                        <td colspan="6" class="text-center">No transactions found.</td>
-                                    </tr>
-                                @endforelse
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                </div>
-                <!-- End Transaction History -->
             </div>
 
             <!-- Right Section (Image) -->
-            <div class="col-lg-3 d-flex">
-                <div class="card flex-grow-1">
+            <div class="col-lg-3">
+                <div class="card">
                     <div class="card-body">
                         @forelse($product->getMedia('images') as $media)
                             <img src="{{ $media->getUrl() }}" alt="Product Image" class="img-fluid img-thumbnail mb-2">
@@ -138,5 +98,45 @@
                 </div>
             </div>
         </div>
+
+        <!-- Transaction History -->
+        <div class="card mt-4">
+            <div class="card-header">
+                <h5>Transaction History</h5>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Type</th>
+                            <th>Quantity</th>
+                            <th>Current Quantity</th>
+                            <th>Location</th>
+                            <th>Reason</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @forelse($product->transactions as $transaction)
+                            <tr>
+                                <td>{{ $transaction->formatted_created_at }}</td>
+                                <td>{{ $transaction->type }}</td>
+                                <td>{{ $transaction->quantity }}</td>
+                                <td>{{ $transaction->current_quantity }}</td>
+                                <td>{{ $transaction->location->name ?? 'N/A' }}</td>
+                                <td>{{ $transaction->reason ?? 'N/A' }}</td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="6" class="text-center">No transactions found.</td>
+                            </tr>
+                        @endforelse
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <!-- End Transaction History -->
     </div>
 @endsection
