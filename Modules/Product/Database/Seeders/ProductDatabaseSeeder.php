9b56a2b6a1ada29e721bcb4172722e5cca5300b9
yusronandrian <59106071+yusronandrian@users.noreply.github.com>
2024-09-09 22:13:28 +0700

    [SCRUM-46] fixing bug seeder product categories

--- Modules/Product/Database/Seeders/ProductDatabaseSeeder.php
+++ Modules/Product/Database/Seeders/ProductDatabaseSeeder.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 use Modules\Product\Entities\Category;
 use Modules\Setting\Entities\Unit;
+use App\Models\User;
+use Modules\Setting\Entities\Setting;
 
 class ProductDatabaseSeeder extends Seeder
 {
@@ -18,9 +20,14 @@ class ProductDatabaseSeeder extends Seeder
     {
         Model::unguard();
 
+        $superAdmin = User::where('email', 'super.admin@test.com')->firstOrFail();
+        $setting = Setting::where('company_name', 'Tiga Saudara ERP')->firstOrFail();
+
         Category::create([
             'category_code' => 'CA_01',
-            'category_name' => 'Random'
+            'category_name' => 'Random',
+            'created_by' => $superAdmin->id,
+            'setting_id' => $setting->id
         ]);
 
         Unit::create([
