d73024e0e001c6a7b568b50397e13d1f6f836cdc
aulaleslie <134106027+aulaleslie@users.noreply.github.com>
2024-09-12 02:10:45 +0800

    [SCRUM-66] bugs: create and update supplier (#12)
    Co-authored-by: JMawansyah <jmawansyah@gmail.com>

--- Modules/People/Resources/views/suppliers/show.blade.php
+++ Modules/People/Resources/views/suppliers/show.blade.php
@@ -19,75 +19,59 @@
                         <div class="table-responsive">
                             <table class="table table-bordered">
                                 <tr>
-                                    <th>Supplier Name</th>
-                                    <td>{{ $supplier->supplier_name }}</td>
+                                    <th>Nama Pemasok</th>
+                                    <td>{{ $supplier->contact_name }}</td>
                                 </tr>
                                 <tr>
-                                    <th>Supplier Email</th>
-                                    <td>{{ $supplier->supplier_email }}</td>
+                                    <th>Identitas</th>
+                                    <td>{{ $supplier->identity }}</td>
+                                </tr>
+                                <tr>
+                                    <th>Nomor Identitas</th>
+                                    <td>{{ $supplier->identity_number ?? '-' }}</td>
                                 </tr>
                                 <tr>
                                     <th>Supplier Phone</th>
                                     <td>{{ $supplier->supplier_phone }}</td>
                                 </tr>
+
                                 <tr>
-                                    <th>Contact Name</th>
-                                    <td>{{ $supplier->contact_name ?? '-' }}</td>
-                                </tr>
-                                <tr>
-                                    <th>Identity</th>
-                                    <td>{{ $supplier->identity ?? '-' }}</td>
-                                </tr>
-                                <tr>
-                                    <th>Identity Number</th>
-                                    <td>{{ $supplier->identity_number ?? '-' }}</td>
-                                </tr>
-                                <tr>
-                                    <th>Fax</th>
-                                    <td>{{ $supplier->fax ?? '-' }}</td>
+                                    <th>Nama Perusahaan</th>
+                                    <td>{{ $supplier->supplier_name ?? '-' }}</td>
                                 </tr>
                                 <tr>
                                     <th>NPWP</th>
                                     <td>{{ $supplier->npwp ?? '-' }}</td>
                                 </tr>
                                 <tr>
-                                    <th>City</th>
-                                    <td>{{ $supplier->city }}</td>
+                                    <th>Alamat Penagihan</th>
+                                    <td>{{ $supplier->billing_address }}</td>
                                 </tr>
                                 <tr>
-                                    <th>Country</th>
-                                    <td>{{ $supplier->country }}</td>
-                                </tr>
-                                <tr>
-                                    <th>Address</th>
-                                    <td>{{ $supplier->address }}</td>
-                                </tr>
-                                <tr>
-                                    <th>Billing Address</th>
-                                    <td>{{ $supplier->billing_address ?? '-' }}</td>
-                                </tr>
-                                <tr>
-                                    <th>Shipping Address</th>
-                                    <td>{{ $supplier->shipping_address ?? '-' }}</td>
+                                    <th>Alamat Pengiriman</th>
+                                    <td>{{ $supplier->shipping_address }}</td>
                                 </tr>
                                 <!-- Bank Information -->
                                 <tr>
-                                    <th>Bank Name</th>
+                                    <th>Nama Bank</th>
                                     <td>{{ $supplier->bank_name ?? '-' }}</td>
                                 </tr>
                                 <tr>
-                                    <th>Bank Branch</th>
+                                    <th>Kantor Cabang Bank</th>
                                     <td>{{ $supplier->bank_branch ?? '-' }}</td>
                                 </tr>
                                 <tr>
-                                    <th>Account Number</th>
+                                    <th>Nomor Rekening</th>
                                     <td>{{ $supplier->account_number ?? '-' }}</td>
                                 </tr>
                                 <tr>
-                                    <th>Account Holder</th>
+                                    <th>Pemegang Akun Bank</th>
                                     <td>{{ $supplier->account_holder ?? '-' }}</td>
                                 </tr>
                             </table>
+                        </div>
+                        <div class="mt-3">
+                            <a href="{{ route('suppliers.index') }}" class="btn btn-secondary">Kembali</a>
                         </div>
                     </div>
                 </div>
