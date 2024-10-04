cf336b0814a78247677cd5877baa4ab0371d80d7
aulaleslie <134106027+aulaleslie@users.noreply.github.com>
2024-10-04 10:02:49 +0800

    [REFACTOR] Product, Adjustment, and Purchase (#31)
    * feat: Add product_stocks table with references to products, locations, and taxes
    
    * feat: Create ProductStock model with relationships to Product, Location, and Tax
    
    * chore: refactor product and add purchase

---
+++ Modules/Product/Http/Requests/InitializeProductStockRequest.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace Modules\Product\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Log;
+
+class InitializeProductStockRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        // Check if the user has permission to create products
+        return Gate::allows('create_products');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'quantity' => ['required', 'integer', 'gt:0'],
+            'quantity_non_tax' => ['required', 'integer', 'min:0'],
+            'quantity_tax' => ['required', 'integer', 'min:0'],
+            'broken_quantity_non_tax' => ['required', 'integer', 'min:0'],
+            'broken_quantity_tax' => ['required', 'integer', 'min:0'],
+            'last_purchase_price' => ['required', 'numeric', 'min:0'],
+            'average_purchase_price' => ['required', 'numeric', 'min:0'],
+            'sale_price' => ['required', 'numeric', 'min:0'],
+            'location_id' => ['required', 'integer', 'exists:locations,id'],
+
+            // Custom rule to validate that the total of the quantities is equal to the overall quantity
+            'total_quantity_check' => function ($attribute, $value, $fail) {
+                $totalQuantity = $this->input('quantity');
+                $sumOfQuantities = $this->input('quantity_non_tax') + $this->input('quantity_tax')
+                    + $this->input('broken_quantity_non_tax') + $this->input('broken_quantity_tax');
+
+                if ($sumOfQuantities !== (int)$totalQuantity) {
+                    $fail('Jumlah total stok (termasuk non-tax, tax, dan broken) harus sama dengan jumlah stok yang dimasukkan.');
+                }
+            },
+        ];
+    }
+
+    /**
+     * Customize the error messages.
+     *
+     * @return array
+     */
+    public function messages(): array
+    {
+        return [
+            'quantity.required' => 'Jumlah stok diperlukan.',
+            'quantity.gt' => 'Jumlah stok harus lebih besar dari 0.',
+            'quantity_non_tax.required' => 'Jumlah stok non-PPN diperlukan.',
+            'quantity_tax.required' => 'Jumlah stok PPN diperlukan.',
+            'broken_quantity_non_tax.required' => 'Jumlah stok rusak non-PPN diperlukan.',
+            'broken_quantity_tax.required' => 'Jumlah stok rusak PPN diperlukan.',
+            'quantity_non_tax.min' => 'Jumlah stok non-PPN tidak boleh kurang dari 0.',
+            'quantity_tax.min' => 'Jumlah stok PPN tidak boleh kurang dari 0.',
+            'broken_quantity_non_tax.min' => 'Jumlah stok rusak non-PPN tidak boleh kurang dari 0.',
+            'broken_quantity_tax.min' => 'Jumlah stok rusak PPN tidak boleh kurang dari 0.',
+            'last_buy_price.required' => 'Harga beli terakhir diperlukan.',
+            'last_buy_price.min' => 'Harga beli terakhir tidak boleh kurang dari 0.',
+            'average_buy_price.required' => 'Harga beli rata-rata diperlukan.',
+            'average_buy_price.min' => 'Harga beli rata-rata tidak boleh kurang dari 0.',
+            'sale_price.required' => 'Harga jual diperlukan.',
+            'sale_price.min' => 'Harga jual tidak boleh kurang dari 0.',
+            'location_id.required' => 'Lokasi produk harus dipilih.',
+            'location_id.exists' => 'Lokasi yang dipilih tidak valid.',
+        ];
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param Validator $validator
+     * @throws HttpResponseException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        Log::info('Validation input:', $this->input());
+        Log::error('Validation failed', $validator->errors()->toArray());
+
+        throw new HttpResponseException(
+            redirect()->back()->withErrors($validator)->withInput()
+        );
+    }
+}
--- Modules/Product/Routes/web.php
+++ Modules/Product/Routes/web.php
@@ -7,15 +7,19 @@
 */
 
 use Illuminate\Support\Facades\Route;
+use Modules\Product\Http\Controllers\ProductController;
 
 Route::group(['middleware' => ['auth', 'role.setting']], function () {
     //Print Barcode
     Route::get('/products/print-barcode', 'BarcodeController@printBarcode')->name('barcode.print');
     Route::get('/products/upload', 'ProductController@uploadPage')->name('products.upload.page');
     Route::post('/products/upload', 'ProductController@upload')->name('products.upload');
-    Route::post('products/store-and-redirect', 'ProductController@storeProductAndRedirectToSerialNumberInput')->name('products.storeAndRedirect');
-    Route::get('products/input-serial-number', 'ProductController@inputSerialNumber')->name('products.inputSerialNumber');
-    Route::post('products/save-serial-numbers', 'ProductController@saveSerialNumbers')->name('products.saveSerialNumbers');
+    Route::post('products/store-and-redirect', [ProductController::class, 'storeProductAndRedirectToInitializeProductStock'])->name('products.storeProductAndRedirectToInitializeProductStock');
+    Route::get('/products/{product_id}/initialize-stock', [ProductController::class, 'initializeProductStock'])->name('products.initializeProductStock');
+    Route::post('/products/{product_id}/initialize-stock', [ProductController::class, 'storeInitialProductStock'])->name('products.storeInitialProductStock');
+    Route::post('/products/{product_id}/initialize-stock-and-redirect', [ProductController::class, 'storeInitialProductStockAndRedirectToInputSerialNumbers'])->name('products.storeInitialProductStockAndRedirectToInputSerialNumbers');
+    Route::get('/products/{product_id}/input-serial-numbers', [ProductController::class, 'inputSerialNumbers'])->name('products.inputSerialNumbers');
+    Route::post('products/{product_id}/input-serial-numbers', [ProductController::class, 'storeSerialNumbers'])->name('products.storeSerialNumbers');
     //Product
     Route::resource('products', 'ProductController');
     //Product Category
