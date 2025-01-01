f9b522c434707fbb26629775ef797cfdae2e3f81
aulaleslie <134106027+aulaleslie@users.noreply.github.com>
2025-01-01 13:21:48 +0800

    Sprint 4/bugs fixes (#60)
    * fix: bugs feedback
    
    * chore: fix translation
    
    * chore: fix purchase
    
    * chore: expose login endpoint
    
    * fix: purchase receive
    
    * chore: fix broken_quantity issue.
    
    * chore: customer name

--- Modules/Sale/Resources/views/create.blade.php
+++ Modules/Sale/Resources/views/create.blade.php
@@ -39,7 +39,7 @@
                                             <label for="customer_id">Pelanggan <span class="text-danger">*</span></label>
                                             <select class="form-control" name="customer_id" id="customer_id" required>
                                                 @foreach(\Modules\People\Entities\Customer::all() as $customer)
-                                                    <option value="{{ $customer->id }}">{{ $customer->customer_name }}</option>
+                                                    <option value="{{ $customer->id }}">{{ $customer->contact_name }}</option>
                                                 @endforeach
                                             </select>
                                         </div>
