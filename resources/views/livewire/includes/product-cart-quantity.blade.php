4c5a7af3a5e1771a3b6f360114d8e559aab63f13
aulaleslie <134106027+aulaleslie@users.noreply.github.com>
2025-10-07 16:29:28 +0800

    Enhance POS checkout pricing metadata (#120)

--- resources/views/livewire/includes/product-cart-quantity.blade.php
+++ resources/views/livewire/includes/product-cart-quantity.blade.php
@@ -1,9 +1,13 @@
+@php
+    $cartOptions = $cart_item->options->toArray();
+    $cartKey = $cartOptions['cart_key'] ?? $cart_item->id;
+@endphp
 <div class="input-group d-flex justify-content-center">
-    <input wire:model="quantity.{{ $cart_item->id }}"
+    <input wire:model="quantity.{{ $cartKey }}"
            style="min-width: 40px; max-width: 90px;"
            type="number"
            class="form-control text-right"
            value="{{ $cart_item->qty }}"
            min="1"
-           wire:blur="updateQuantity('{{ $cart_item->rowId }}', {{ $cart_item->id }})">
+           wire:blur="updateQuantity('{{ $cart_item->rowId }}', '{{ $cartKey }}')">
 </div>
